3a02319a4f548785c4a42004f9db051f61df2e21
Dr. Carsten Leue <carsten.leue@gmx.net>
2019-12-09 22:47:20 +0100

    initial checkin

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+function toLowerCase(aValue: string): string {
+    return aValue.toLowerCase();
+}
+
+export const SIMPLE_CONSTANT = 'SIMPLE_CONSTANT';
+
+export const DERIVED_CONSTANT = toLowerCase(SIMPLE_CONSTANT);
